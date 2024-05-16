092c8be405b6926a8bf4129005cb35921c10b5fa
Quentin <quentin.rivollat@protonmail.com>
2024-05-16 16:27:10 +0200

    Skills section

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,27 @@
 
 
 /*=============== SKILLS TABS ===============*/
+const tabs = document.querySelectorAll("[data-target]"),
+      tabContents = document.querySelectorAll("[data-content]");
 
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove("skills__active");
+        });
+
+        target.classList.add("skills__active");
+
+        tabs.forEach((tab) => {
+          tab.classList.remove("skills__active");
+        });
+
+        tab.classList.add("skills__active");
+
+    });
+})
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
