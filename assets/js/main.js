a95b5c62a8da1e5a30dfcee03287e8bef820f4de
QuentinRiv <quentin.rivollat@protonmail.com>
2024-05-28 15:09:45 +0200

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,23 @@
 /*=============== SHOW SIDEBAR ===============*/
-
+const navMenu = document.getElementById("sidebar");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
 
 /*===== SIDEBAR SHOW =====*/
 /* Validate If Constant Exists */
-
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-sidebar");
+  })
+}
 
 /*===== SIDEBAR HIDDEN =====*/
 /* Validate If Constant Exists */
-
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-sidebar");
+  })
+}
 
 /*=============== SKILLS TABS ===============*/
 const tabs = document.querySelectorAll("[data-target]"),
@@ -106,13 +116,71 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*=============== SWIPER TESTIMONIAL ===============*/
-
+let swiper = new Swiper(".testimonials__container", {
+  spaceBetween: 24,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 48,
+    },
+  }
+});
 
 /*=============== INPUT ANIMATION ===============*/
+const inputs = document.querySelectorAll("input");
 
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if(this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+})
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
 
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+  
+  // get current scroll position
+  let scrollY = window.scrollY;
+  // Now we loop through sections to get height, top and ID values for each
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+    // If our current scroll position enters the space where current section on screen is,
+    // add .active class to corresponding navigation link
+    // else remove it
+    // To know which link needs on active class, we use sectionId variable we are getting
+    // while looping through sections as an selector
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector(`.nav__menu a[href*=${sectionId}]`).classList.add("active-link");
+    } else {
+      document.querySelector(`.nav__menu a[href*=${sectionId}]`).classList.remove("active-link");
+    }
+  })
+}
 
 /*=============== SHOW SCROLL UP ===============*/
 
